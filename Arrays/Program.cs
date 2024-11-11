b8380975b0d66ab545ab1ea42614691a95921fa0
Can Gumus <mzyt.gms@gmail.com>
2024-11-12 00:27:07 +0300

    ref return and arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] d1 = (int[])Array.CreateInstance(typeof(int), 2);
+int[,,] d2 = (int[,,])Array.CreateInstance(typeof(int), 2, 3, 4);
+
+(int a, string b)[] array = new (int a, string b)[]
+{
+    (0, "x"),
+    (1, "y"),
+    (2, "z"),
+    (3, "t"),
+    (4, "v"),
+}; // Tuple
+
+(int a, string b)[] arr =
+[
+    (0, "x"),
+    (0, "y"),
+    (0, "z"),
+    (0, "t"),
+    (0, "v"),
+]; // Tuple
---
+++ RefReturnLocal/Program.cs
@@ -0,0 +1,44 @@
+﻿//int b = 5;
+
+//X(b);
+
+
+//void X(int a)
+//{
+
+//}
+
+////Bu şekilde bellekte hem a hem b değerinde iki değişken oluşur. Bu durumda b veya a da oluşacak 
+////herhangi bir değişiklik birbirlerini etkilemeyecek.
+
+////----------------------------------------------------------------------------------------------------
+
+//int c = 5;
+
+//Y(ref c);
+
+//void Y(ref int a)
+//{
+
+//}
+
+//Burada a ve c aynı bellek adresini işaretlemektedir. Bu da a için yapılacak herhangi bir değişimin
+//c değeri için de gerçekleşeceğini gösterir a artık c dir. a = c yanlıştır. *a = *c doğrudur.
+
+//-----------------------------------------------------------------------------------------------------
+
+int d = 5;
+
+ref int x = ref Z(ref d);
+
+Console.WriteLine(d);
+Console.WriteLine(x);
+d = 787878;
+Console.WriteLine(x);
+
+ref int Z(ref int a)
+{
+    a = 124;
+
+    return ref a;  //ref return örneği
+}
