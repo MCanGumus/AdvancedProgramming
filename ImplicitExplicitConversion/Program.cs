6620caaa8e0a06731ae250c792818d58e2a555d3
Can Gumus <mzyt.gms@gmail.com>
2024-11-12 20:40:42 +0300

    ImplicitExplicit Conversion is finished.

---
+++ ImplicitExplicitConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿class A
+{
+    public int NumberA { get; set; }
+
+    public static explicit operator B(A i) => new B();
+
+}
+
+class B
+{
+    public int NumberB { get; set; }
+
+    public static implicit operator A(B i) => new A();
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        A a = new B();
+
+        A a2 = (A)new B();
+    }
+}
+
