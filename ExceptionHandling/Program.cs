302aa4c2464378058bc92b53aff09782db008021
Can Gumus <mzyt.gms@gmail.com>
2024-11-12 21:12:43 +0300

    Exception Handling is finished.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,42 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            while (true)
+            {
+                var key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.M)
+                {
+                    throw new CustomException("You pressed to key M.") { MyProperty = 5};
+                } //throw ancak Exception ile imzalanmış sınıflarda kullanılabilir.
+                else { Console.WriteLine(key.Key); }
+            }
+        }
+        catch (CustomException ex)
+        {
+
+            throw;
+        }
+       
+    }
+}
+
+class CustomException : Exception
+{
+    public CustomException() : base("Custom exception.")
+    {
+            
+    }
+
+    public CustomException(string message) : base(message)
+    {
+                
+    }
+
+    public int MyProperty { get; set; }
+}
+
