3c8ac5721b4f0e9c59aae7f7b3c0253ebba56085
Can Gumus <mzyt.gms@gmail.com>
2024-11-18 21:59:20 +0300

    Events finished.

---
+++ Events/Example.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class PathControl
+    {
+        public delegate void PathHandler(float sizeMB);
+        public event PathHandler PathControlEvent;
+
+        public async Task ControlPanel(string path)
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                DirectoryInfo directoryInfo = new(path);
+
+                var files = directoryInfo.GetFiles();
+
+                float size = await Task.Run(() => directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories).Sum(f => f.Length));
+
+                float sizeMB = (size / 1024) / 1024;
+
+                if (sizeMB > 50)
+                {
+                    PathControlEvent(sizeMB);
+                }
+            }
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,65 @@
+﻿using Events;
+using System.Globalization;
+
+//Notlar: Delegate'lere instance üzerinden erişilemez. 
+
+//Soru: C#'TA bir sınıfın içerisinde tanımlanmış olan delegate'e instance üzerinden nasıl erişilir.
+//Cevap: Event yapısı ile.
+
+string path = @"PathInfo";
+
+MyEventPublisher myEventPublisher = new MyEventPublisher();
+
+myEventPublisher.MyEvent += X;
+myEventPublisher.MyEvent += X;
+myEventPublisher.MyEvent -= X;
+
+myEventPublisher.RaiseEvent();
+
+MyEventPublisher.XHandler dHandler = new MyEventPublisher.XHandler(X);
+
+//-----------------------------------------------------------------------------
+
+PathControl pathControl = new PathControl();
+
+pathControl.PathControlEvent += (sizeMB) =>
+{
+    Console.WriteLine($"Boyut 50 MB aştı.{sizeMB}");
+};
+
+await pathControl.ControlPanel(path);
+
+//------------------------------------------------------------------------------
+
+void X()
+{
+
+}
+
+class MyEventPublisher
+{
+    public delegate void XHandler();
+
+    XHandler xDelegate;
+
+    public event XHandler MyEvent 
+    {
+        add
+        {
+            Console.WriteLine("Event'e metot bağlandı.");
+            xDelegate += value;
+        }
+        remove
+        {
+            Console.WriteLine("Event'ten metot kaldırıldı.");
+            xDelegate -= value;
+        }
+    }
+
+    public void RaiseEvent()
+    {
+        //MyEvent();
+
+        xDelegate();
+    }
+}
